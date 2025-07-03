298add5c8786fb4f7c13c51546c91eaeb5e36c6a
Robert Juszczyński <robertj.webdev@icloud.com>
2025-07-03 22:55:24 +0200

    feat: Preloader functionality ( animations, chatbot z-index changed )

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,31 @@
+"use client";
+
 import ChatBot from '@/components/chatbot/ChatBot';
-import styles from "./page.module.css";
+import styles from './page.module.css';
 import Header from '@/components/layout/Header/Header';
-import HeroSection from '@/components/layout/Hero';
+import HeroSection from '@/components/layout/Hero/Hero';
+import ExperienceSection from '@/components/layout/Experience/Experience';
+import Preloader from '@/components/layout/Preloader/Preloader';
+import { useState } from 'react';
 
 export default function Home() {
+  const [loaded, setLoaded] = useState(false);
+
+  const onLoaded = () => {
+    setLoaded(true);
+  };
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Header />
-        <HeroSection />
+        <Preloader loaded={onLoaded} />
+        {loaded ? (
+          <>
+            <Header />
+            <HeroSection />
+            {/* <ExperienceSection /> */}
+          </>
+        ) : null}
         <ChatBot />
       </main>
     </div>
---
+++ src/components/layout/Preloader/Preloader.tsx
@@ -0,0 +1,115 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const texts = [
+  'Cześć!',
+  'Hello!',
+  'Hola!',
+  'こんにちは！',
+  'Guten Tag!',
+  'Привет!',
+  '안녕하세요!',
+  'Olá!',
+  'Ciao!',
+  '你好！',
+  'Merhaba!',
+  'Sawa dee!',
+];
+
+type PreloaderProps = {
+  loaded: () => void;
+};
+
+export default function Preloader({ loaded }: PreloaderProps) {
+  const [index, setIndex] = useState(0);
+  const [delay, _] = useState(600);
+  const [visible, isVisible] = useState(true);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    const cycle = (i = 0, currentDelay = 550) => {
+      if (!isMounted) return;
+      setIndex((prev) => (prev + 1) % texts.length);
+
+      const nextDelay = currentDelay > 200 ? currentDelay - 100 : 200;
+
+      setTimeout(() => cycle(i + 1, nextDelay), nextDelay);
+    };
+
+    const timeout = setTimeout(() => cycle(), delay);
+
+    setTimeout(() => {
+      isVisible(false);
+    }, 3000);
+
+    setTimeout(() => {
+      loaded();
+    }, 3500);
+
+    return () => {
+      isMounted = false;
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  return (
+    <AnimatePresence mode='wait'>
+      {visible ? (
+        <motion.div
+          key='textContainer'
+          exit={{ y: '-100%' }}
+          transition={{
+            duration: 1.25,
+            ease: 'easeInOut',
+          }}
+          style={{
+            position: 'absolute',
+            width: '100vw',
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontWeight: '300',
+            zIndex: '1000',
+            backgroundColor: 'var(--foreground)',
+            top: 0,
+            left: 0,
+            overflow: 'hidden',
+          }}
+        >
+          <motion.span
+            key={texts[index]}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.6 }}
+            style={{
+              color: 'var(--secondary-text)',
+              fontSize: '2.2rem',
+              display: 'flex',
+              alignItems: 'center',
+              gap: '1rem',
+              fontWeight: 300,
+            }}
+          >
+            <span
+              style={{
+                fontSize: '3rem',
+                lineHeight: '1',
+                display: 'inline-block',
+                position: 'relative',
+                top: '2px',
+              }}
+            >
+              •
+            </span>
+            {texts[index]}
+          </motion.span>
+        </motion.div>
+      ) : null}
+    </AnimatePresence>
+  );
+}
