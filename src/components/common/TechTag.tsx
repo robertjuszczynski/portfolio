0a762f52b5ade3fc38b5bf6cf8141cf6d18836f5
Robert Juszczyński <robertj.webdev@icloud.com>
2025-07-11 22:12:12 +0200

    feat: TechTag component

---
+++ src/components/common/TechTag.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import styles from './TechTag.module.scss';
+
+type Technology = 'React.js' | 'Vue';
+
+type TechTagProps = {
+  technology: Technology;
+};
+
+export default function TechTag({ technology }: TechTagProps) {
+  const imageName = technology.toLowerCase().replace('.js', '') + '.png';
+  const src = `/images/technologies/${imageName}`;
+
+  return (
+    <div className={styles.techtag}>
+      <Image src={src} alt={technology} width={20} height={20} />
+      <span className={styles.techtag__text}>{technology}</span>
+    </div>
+  );
+}
