12afe7b5cd12cdb615c41f7fd0b65c5c26a54a23
Robert Juszczyński <robertj.webdev@icloud.com>
2025-07-18 23:44:08 +0200

    styles: Changed projects and ribbon bottom margin, changed order of components on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,8 +29,8 @@ export default function Home() {
           <ExperienceSection />
           <Summary />
           <ProjectsSection />
-          <Skills />
           <Ribbon />
+          <Skills />
         </>
       ) : null}
       <ChatBot />
