8e707b5b4fca2ad3744435a331987a866219a34e
Robert Juszczyński <robertj.webdev@icloud.com>
2025-07-29 16:33:45 +0200

    feat: new useMediaQuery hook for using media queries in components

---
+++ src/hooks/useMediaQuery.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+
+export default function useMediaQuery(query: string): boolean {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+    const media = window.matchMedia(query);
+    const listener = () => setMatches(media.matches);
+    setMatches(media.matches);
+    media.addEventListener('change', listener);
+    return () => media.removeEventListener('change', listener);
+  }, [query]);
+
+  return matches;
+}
